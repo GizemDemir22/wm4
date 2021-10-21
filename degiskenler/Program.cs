abd1c13f62cdf0ac24cb3403891ddeac07a72593
Ömer Demir <omer.demir-@hotmail.com>
2021-10-21 23:52:26 +0300

    ilk günüm değişken ve karar yapıları

--- degiskenler/Program.cs
+++ degiskenler/Program.cs
@@ -7,6 +7,39 @@ namespace degiskenler
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            //TAD-tipad=değer
+            //tam sayı değişken tipleri
+            short sayi = 32;
+            int sayi2 = 32;
+            sayi =(short) sayi2;
+
+            //ondalık sayılar
+            double sonuc = 7 / 2;
+            Console.WriteLine(sonuc);
+
+            //stringler
+            string kelimne = "merhaba dünya";
+            Console.WriteLine(kelimne);
+            char sonHarf = 'z';
+            Console.WriteLine(sonHarf);
+            // mantıksal değişken tipi!!!
+
+            bool isactive = true;
+            isactive = !isactive;
+            Console.WriteLine(isactive);
+
+            DateTime tarih = DateTime.Now;
+            Console.WriteLine(tarih.DayOfWeek);
+
+
+
+            
+
+
+
+           
+
+            
         }
     }
 }
---
+++ karar yapıları/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace karar_yapıları
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isactive = true;
+            if (isactive)
+            {
+                Console.WriteLine("aktif");
+            }
+            else
+            {
+                Console.WriteLine("aktif değil");
+            }
+
+            // else if
+            int not = 69;
+
+
+            if (not >= 70)
+            {
+                Console.WriteLine("geçti");
+
+            } else if (not < 70 & not > 50) 
+            {
+                Console.WriteLine("orta");
+            }else if (not<=50 & not>30)
+            {
+                Console.WriteLine("geçerli");
+            }
+            else
+            
+            {
+                Console.WriteLine("kaldı");
+
+            }
+
+            //switch-case
+
+            string sonuc = "pazar";
+            switch(sonuc)
+            {
+                case "pazartesi":
+                    Console.WriteLine("pazartesi");
+                    break;
+                case "salı":
+                    Console.WriteLine("salı");
+                    break;
+
+                default:
+
+                    Console.WriteLine("geçersiz gün");
+                    break;
+
+                 
+
+            }
+
+            //donguler
+            //while
+
+            DateTime tarih = DateTime.Now;
+            int gunsayisi = 0;
+            int sayac = 0;
+
+            while (gunsayisi < 100)
+            {
+                DateTime sonrakigun = tarih.AddDays(sayac);
+                sayac++;
+                if (sonrakigun.DayOfWeek == DayOfWeek.Saturday || sonrakigun.DayOfWeek == DayOfWeek.Sunday)
+                    continue;
+                Console.WriteLine(sonrakigun);
+                gunsayisi++;
+            }
+
+        }
+    }
+}
